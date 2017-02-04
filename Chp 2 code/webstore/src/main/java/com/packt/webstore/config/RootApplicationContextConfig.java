db9bdee1584781eed346bb2b236dbddf0cdd758b
Satoshi Ohtsuka <sohtsuka1317@gmail.com>
2017-02-04 21:08:31 +0900

    Modify Chapter 2 to use Spring Boot and Thymeleaf

--- Chp 2 code/webstore/src/main/java/com/packt/webstore/config/RootApplicationContextConfig.java
+++ Chp 2 code/webstore/src/main/java/com/packt/webstore/config/RootApplicationContextConfig.java
@@ -3,7 +3,6 @@ package com.packt.webstore.config;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
@@ -11,7 +10,6 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
-@ComponentScan("com.packt.webstore")
 public class RootApplicationContextConfig {
 
    @Bean
--- Chp 2 code/webstore/src/main/java/com/packt/webstore/controller/ProductController.java
+++ Chp 2 code/webstore/src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,12 +1,10 @@
 package com.packt.webstore.controller;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.packt.webstore.domain.Product;
+
 import com.packt.webstore.domain.repository.ProductRepository;
 import com.packt.webstore.service.ProductService;
 
