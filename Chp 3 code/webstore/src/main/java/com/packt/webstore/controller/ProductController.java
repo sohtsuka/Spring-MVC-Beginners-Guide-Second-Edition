b862c14cc8c45388d8b044ab4d246cf70b93f591
Satoshi Ohtsuka <sohtsuka1317@gmail.com>
2017-02-04 22:35:54 +0900

    Modify Chapter 3 to use Spring Boot and Thymeleaf

--- Chp 3 code/webstore/src/main/java/com/packt/webstore/controller/ProductController.java
+++ Chp 3 code/webstore/src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.packt.webstore.controller;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.packt.webstore.domain.Product;
-import com.packt.webstore.domain.repository.ProductRepository;
 import com.packt.webstore.service.ProductService;
 
 @Controller
